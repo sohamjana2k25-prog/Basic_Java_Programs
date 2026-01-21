537b893dde63ef44a21114ed16503f79dc55ddb6
Soham Jana <sohamjana2k25@gmail.com>
2026-01-21 21:51:36 +0530

    Rename class from Largest_ 3_Numbers to 3numlargest

--- basic loops/3numlargest.java
+++ basic loops/3numlargest.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Main  {
+public class 3numlargest {
     public static void main(String[] args) {
         Scanner inp = new Scanner(System.in);
         int largest=0;
